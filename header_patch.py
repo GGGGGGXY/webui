6eda90c901797f165bc4b2c455a462c46551fe3f
GAOXinyu <rizia1996@outlook.com>
2023-01-12 15:27:02 +0800

    FEAT: support taiyi v2

---
+++ header_patch.py
@@ -0,0 +1,8 @@
+      with gr.Box(visible=is_spaces):
+            gr.HTML(f'''
+                    <div class="gr-prose" style="max-width: 80%">
+                        <p>🚧 (WIP) Private Automatic1111 Stable Diffusion Web UI on 🤗 Hugging Face Spaces : Running Model: IDEA-CCNL/Taiyi-Stable-Diffusion-1B-Chinese-v2.1-base</p>
+                        <p>You can learn more in our github 📺 <a href="https://github.com/IDEA-CCNL/Fengshenbang-LM" style="target=" _blank"="">https://github.com/IDEA-CCNL/Fengshenbang-LM</a></p> 
+                    </div>
+                ''')
+
